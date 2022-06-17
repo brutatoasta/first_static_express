98c48809c5284f7b727894fa80335fdeeef263d4
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-06-17 14:49:57 +0800

    add image

--- node-app-esc.js
+++ node-app-esc.js
@@ -7,6 +7,7 @@ const port = 3000;
 // });
 
 app.use(express.static('public'));
+app.use(express.static('images'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
