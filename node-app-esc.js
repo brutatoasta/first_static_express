c7191d64e052fe04b38309c84c511812636056df
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-06-17 15:58:24 +0800

    add css and bg image

--- node-app-esc.js
+++ node-app-esc.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('My name is (G)root.');
-// });
-
 app.use(express.static('public'));
 app.use(express.static('images'));
+app.use(express.static('css'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
