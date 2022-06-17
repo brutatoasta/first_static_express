dfc8c72c92d7c85526e60df312c14297f2012b08
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-06-17 17:13:56 +0800

    add error 404

--- node-app-esc.js
+++ node-app-esc.js
@@ -5,6 +5,10 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.static('images'));
 app.use(express.static('css'));
+// Handling non matching request from the client
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Error 404: Page not found on the server</h1>")});
 
 app.listen(port, () => {
     console.log(`Awesome app listening on port ${port}`)
